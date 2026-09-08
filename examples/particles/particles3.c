1ef90b3a24dd32b0ee5dc3729d1517bc84067199
zzc <zzc@lenovo>
2026-09-09 01:36:27 +0800

    添加examples程序

---
+++ examples/particles/particles3.c
@@ -0,0 +1,32 @@
+/*
+  This file is part of p4est.
+  p4est is a C library to manage a collection (a forest) of multiple
+  connected adaptive quadtrees or octrees in parallel.
+
+  Copyright (C) 2010 The University of Texas System
+  Additional copyright (C) 2011 individual authors
+  Written by Carsten Burstedde, Lucas C. Wilcox, and Tobin Isaac
+
+  p4est is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  p4est is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with p4est; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/*
+ * We compile the 3D example from the same code as the 2D example.
+ * We just redefine the mesh functions from 2D to 3D and use a few
+ * `#ifdef P4_TO_P8` branches to switch from 2D- to 3D-specific code.
+ */
+
+#include <p4est_to_p8est.h>
+#include "particles2.c"
